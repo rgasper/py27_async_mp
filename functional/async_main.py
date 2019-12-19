ea7ad19c3c0be57c881d77abafab14867f519a91
Raymond Gasper <rgasper@netprospex.com>
2019-12-19 08:07:12 -0500

    functional style async pipeline

---
+++ functional/async_main.py
@@ -0,0 +1,124 @@
+'''
+async_main.py
+a simple test application to validate this multiprocess design
+usage: $ python async_main.py num_elems num_procs
+Author: Raymond Gasper
+'''
+
+import argparse
+from threading import Thread
+from multiprocessing import Process, Queue, Value
+from multiprocessing_logging import install_mp_handler
+from logging import info, debug, exception
+from logging.config import dictConfig
+from time import time
+
+from async_mp_cores import producer, manager, worker, consumer
+
+log_config = {
+    "version": 1,
+    "disabled_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(module)s:%(funcName)s:%(lineno)d] %(asctime)s [PID:%(process)d] [THREAD:%(threadName)s] - [%(levelname)s] \n %(message)s",
+        },
+        "default": {
+            "format": "[%(module)s:%(funcName)s:%(lineno)d] %(asctime)s - [%(levelname)s] - %(message)s",
+        },
+    },
+    "handlers": {
+        "daily": {
+            # daily log is for high-level application health information
+            "level"       : "INFO",
+            "class"       : "logging.handlers.TimedRotatingFileHandler",
+            "formatter"   : "default",
+            "filename"    : 'log_timed',
+            "when"        : "H",
+            "backupCount" : 14,
+        },
+        "debug": {
+            # debug logger is for high-volume stuff that's unimportant if the app is
+            # working but so very clutch to see if the app breaks. 
+            "level"       : "DEBUG",
+            "class"       : "logging.handlers.RotatingFileHandler",
+            "formatter"   : "verbose",
+            "filename"    : 'log_rotating',
+            "backupCount" : 1, # 0 induces handler to never rotate the file
+            "maxBytes"     : 1024*1024*256 # 256 MB
+        },
+        "stream": {
+            "level"     : "NOTSET",
+            "class"     : "logging.StreamHandler",
+            "formatter" : "default",
+        },
+    },
+    "loggers": {
+        # root | emptystr is a special name that allows any logger in the application to use it
+        # if you use a loggger with a different name, imported libraries may not log properly
+        "": {
+            "handlers" : ["stream","debug","daily"],
+            "level"    : "DEBUG", # send everything to handlers
+        },
+    },
+}
+dictConfig(log_config)
+# install multiprocess log handlers on root logger by default
+install_mp_handler() 
+
+def pipeline(num_elems, num_procs):
+    info('setting up')
+    results_queue = Queue()
+    input_queue   = Queue()
+    poisonPill = Value('i',0)
+    producer_p = Process(
+        target = producer,
+        args   = (
+            args.num_elems,
+            input_queue,
+        ),
+    )
+    manager_p = Process(
+        target = manager,
+        args   = (
+            input_queue,
+            args.num_procs,
+            poisonPill, 
+            worker,
+            (input_queue, results_queue, poisonPill),
+        ),
+    )
+    consumer_p = Process(
+        target = consumer,
+        args   = (
+            results_queue,
+            args.num_elems,
+        )
+    )
+    info('running pipeline')
+    start_time = time()
+    producer_p.start()
+    manager_p.start()
+    consumer_p.start()
+    producer_p.join()
+    info('asking main to stop')
+    with poisonPill.get_lock():
+        poisonPill.value = int(True)
+    manager_p.join()
+    consumer_p.join()
+    info('pipeline finished')
+    dur = time() - start_time
+    info('pipeline took {d:2f}s to process {i} elements with {n} parallel processes'.format(
+        d = dur,
+        i = args.num_elems,
+        n = args.num_procs
+    ))
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num_elems', type=int)
+    parser.add_argument('num_procs', type=int)
+    args = parser.parse_args()
+    
+    pipeline(args.num_elems, args.num_procs)
+    
---
+++ functional/async_mp_cores.py
@@ -0,0 +1,99 @@
+'''
+async_mp_cores.py
+work and management functions for a bounded (in memory and cpu usage), logged, 
+and fully asynchronous multiprocess ETL application
+design prevents unbounded memory/process number growth that can occur in multiprocess ETL python applications
+process creation and death overhead will dominate if work elements are small
+Author: Raymond Gasper
+'''
+from logging import debug, info
+from multiprocessing import Process
+from time import sleep, time
+from queue import Empty
+from math import log as ln
+
+def producer(num_elems, queue):
+    ''' pushes stuff into the queue '''
+    info('starting')
+    for _ in range(num_elems):
+        queue.put(range(1000))
+
+
+def worker(in_queue, out_queue, poisonPill):
+    ''' grabs input, does some work, and pushes results. ping-pongs the poisonPill '''
+    i = in_queue.get()
+    debug('doing work')
+    j = [x**2 for x in i]
+    out_queue.put(j)
+    return
+
+def consumer(out_queue, expected_num_results):
+    ''' does something with the ETL results '''
+    r_count = 0
+    while True:
+        _ = out_queue.get()
+        r_count += 1
+        debug('result gotten')
+        if r_count == expected_num_results:
+            info('finished with expected number of result elements')
+            return
+
+
+def manager(in_queue, n_processes, poisonPill, worker, worker_args):
+    ''' manages a set of concurrent processes defined by target and args 
+    does not deliver or return any data 
+    :params:
+        in_queue: multiprocessing.Queue- input data for workers. manager observes queue status
+        n_processes: int- number concurrent processes
+        poisonPill: multiprocessing.Value- flag provided that indicates no further input data
+        target: function- worker function. Will be daemonized, so can have no child processes
+        worker_args: tuple- arguments for worker function'''
+    # keep constant size objects, or pool can be a source of memory issues
+    pool = {i:None for i in range(n_processes)}
+    proc_time_tracker = {i:0 for i in range(n_processes)}
+    n_procs, avg_duration = 0, 0.0
+    empty_repeat_limit, repeats = int(ln(n_processes)*2)+1, 0
+    info('managing')
+    while True:
+        sleep(max(0.1, 2*avg_duration/float(n_processes)))
+        if bool(poisonPill.value):
+            if in_queue.empty():
+                repeats += 1
+            if repeats > 0:
+                debug('{} consecutive empty queue checks'.format(repeats))
+            if repeats > empty_repeat_limit:
+                break
+        for i, p in pool.iteritems():
+            if p is None:
+                debug('starting a worker')
+                new_p = Process(
+                    target = worker,
+                    args = worker_args,
+                )
+                new_p.daemon = True
+                new_p.start()
+                proc_time_tracker[i] = time()
+                pool[i] = new_p
+            elif not p.is_alive():
+                worker_duration = time() - proc_time_tracker[i]
+                debug('replacing a worker')
+                new_p = Process(
+                    target = worker,
+                    args = worker_args,
+                )
+                new_p.daemon = True
+                new_p.start()
+                proc_time_tracker[i] = time()
+                pool[i] = new_p
+                # update duration statistic
+                n_procs += 1
+                diff = worker_duration - avg_duration
+                avg_duration += float(diff)/n_procs
+
+    info('all work consumed, shutting down')
+    info('manager done, average worker element process time: {:.5f}s'.format(avg_duration))
+    return
+
+
+if __name__ == "__main__":
+    raise NotImplementedError('use async_main.py')
